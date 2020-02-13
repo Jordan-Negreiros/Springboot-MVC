96ac704232c971b9177a38688d683c537775c5c7
Jordan Negreiros <jordannegreirossantos@gmail.com>
2020-02-13 20:31:17 -0300

    Criando projeto e testando index.html

---
+++ src/test/java/com/jordan/springboot/ProjectspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordan.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
