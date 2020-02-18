40b1176e542f7ad2cc3cacfe3776095c1b4b197d
Jordan Negreiros <jordannegreirossantos@gmail.com>
2020-02-18 16:51:31 -0300

    Consumindo Web Service RESTful CEP

--- src/main/java/com/jordan/model/Pessoa.java
+++ src/main/java/com/jordan/model/Pessoa.java
@@ -22,6 +22,13 @@ public class Pessoa implements Serializable {
     @NotNull(message = "Sobrenome não pode ser Nulo")
     @NotEmpty(message = "Sobrenome não pode ser Vazio")
     private String sobrenome;
+    
+    private String cep;
+    private String rua;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String ibge;
 
     @Min(value = 18, message = "Idade inválida")
     private int idade;
@@ -68,5 +75,55 @@ public class Pessoa implements Serializable {
     public void setSobrenome(String sobrenome) {
         this.sobrenome = sobrenome;
     }
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getIbge() {
+		return ibge;
+	}
+
+	public void setIbge(String ibge) {
+		this.ibge = ibge;
+	}
+    
+    
 }
 
